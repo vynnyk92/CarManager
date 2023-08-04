16acc43eda1b2200f82f5f394c8ad3f89ddffe00
pylyp.vynnykov <pylyp.vynnykov@justeattakeaway.com>
2023-08-04 21:38:37 +0200

    Add project files.

---
+++ CarManager.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace CarManager.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ test/CarManager.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace CarManager.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
