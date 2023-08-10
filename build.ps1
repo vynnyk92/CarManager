cba772d66f0abee81ceb212abdbdc33bcf19f13f
pylyp.vynnykov <pylyp.vynnykov@justeattakeaway.com>
2023-08-11 00:02:39 +0200

    logs

--- build.ps1
+++ build.ps1
@@ -3,14 +3,13 @@ function DotNetTest {
         [string] $Project
     )
     
-    try {
-        & dotnet test `
-            $Project `
-            -- RunConfiguration.TestSessionTimeout=900000
-    }
-    finally {
-        $env:environment = $oldEnvironment
-    }
+    Write-Output "Before test project: $Project"
+        
+    & dotnet test `
+        $Project `
+        -- RunConfiguration.TestSessionTimeout=900000
+
+    Write-Output "After test project: $Project"
 
     if ($LASTEXITCODE -ne 0) {
         throw "dotnet test failed with exit code $LASTEXITCODE"
